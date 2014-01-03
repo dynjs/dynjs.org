06f76772df20f8c204f18b50ff7bcabfa98cf0fe
Lance Ball <lball@redhat.com>
2014-01-03 13:37:31 -0500

    Add release notes generator, and first stab at release notes for 0.2.1

---
+++ generate-release-data.js
@@ -0,0 +1,48 @@
+/**
+ * Generates a JSON data file for Harp that contains the issues resolved for a
+ * given milestone. Required variables are the milestone number and output dir.
+ */
+var GitHub = require('github');
+var FileSystem = require('fs');
+
+
+var milestone  = 1;     // Use the milestone number from github.com/dynjs/dynjs/issues/milestones
+var outputDir  = 'v0_2_1'; // The version number. Assumes a prefix of public/releases
+
+var baseDir    = 'public/releases';
+var releaseDir = [baseDir, outputDir].join('/');
+var dataFile   = [baseDir, outputDir, '_data.json'].join('/');
+
+checkDir();
+
+var github = new GitHub({
+  version: "3.0.0",
+    debug: true
+});
+
+github.issues.repoIssues({
+  user: "dynjs",
+  repo: "dynjs",
+  milestone: milestone,
+  state: "closed"
+}, function(err, data) {
+  if (err) {
+    console.log("There was an error fetching the issues");
+  } else {
+    writeData(data);
+  }
+});
+
+function writeData(data) {
+  FileSystem.writeFile(dataFile, JSON.stringify({issues: data}));
+  console.log(data);
+}
+
+function checkDir() {
+  FileSystem.exists(releaseDir, function(exists) {
+    if (!exists) {
+      console.log(releaseDir + " does not exist. Creating it.");
+      FileSystem.mkdirSync(releaseDir);
+    }
+  });
+}
